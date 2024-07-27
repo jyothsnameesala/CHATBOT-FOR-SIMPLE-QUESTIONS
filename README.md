5053bf86efda4e44ef0551d26a43d8aded454f0d
jyothsnameesala <jyothsnameesala2003@gmail.com>
2024-07-27 14:10:40 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+project:chatbot for simple questions
+ Getting Started 
+ Download all the files
+ Open Command Prompt
+ Get the control to the folder where your files are present
+ Make sure your system has the following modules installed - tensorflow, keras, nltk, pickle. 
+ If not, then install the modules using the command - {pip install module_name}.
+ If your system already has the module, it will show "Requirement already satisfied".
+ Now we have to train and create the model. 
+ Hence execute the "train_chatbot.py" file using the following command - {python train_chatbot.py}.
+ If the training is successful it will show model created. 
+ To open the GUI window and start conversation with the chatbot execute the "chatgui.py" file using the following command - {python chatgui.py}. 
+ It will open the GUI  window. 
+ Write your text in the section on the right to the send button and click on "Send". Enjoy the responses !  
---
+++ train_chatbot.py
@@ -0,0 +1,97 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import json
+import pickle
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout, Input
+from keras.optimizers import SGD
+import random
+
+# Load intents file
+data_file = open('intents.json').read()
+intents = json.loads(data_file)
+
+words = []
+classes = []
+documents = []
+ignore_words = ['?', '!']
+
+# Process intents
+for intent in intents['intents']:
+    for pattern in intent["patterns"]:
+        # Tokenize each word
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+        # Add documents in the corpus
+        documents.append((w, intent['tag']))
+        # Add to our classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+# Lemmatize and lower each word and remove duplicates
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+
+# Sort classes
+classes = sorted(list(set(classes)))
+
+# Print information
+print(len(documents), "documents")
+print(len(classes), "classes", classes)
+print(len(words), "unique lemmatized words", words)
+
+# Save words and classes
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+# Create our training data
+training = []
+output_empty = [0] * len(classes)
+
+for doc in documents:
+    bag = []
+    pattern_words = doc[0]
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+    
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+    training.append([bag, output_row])
+
+# Shuffle our features and turn into np.array
+random.shuffle(training)
+
+# Print shapes of elements
+for item in training:
+    print(len(item), len(item[0]), len(item[1]))
+
+# Ensure consistent shapes
+training = np.array(training, dtype=object)
+
+# Create train and test lists. X - patterns, Y - intents
+train_x = np.array([i[0] for i in training])
+train_y = np.array([i[1] for i in training])
+
+print("Training data created")
+
+# Create model
+model = Sequential()
+model.add(Input(shape=(len(train_x[0]),)))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Compile model
+sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# Fit and save the model
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+
+print("model created")
